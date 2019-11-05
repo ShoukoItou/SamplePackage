284daf56d6cdeb61847f274f67f61b5fa5c635ce
S.Itou <Shouko.Itou-N467@nid.co.jp>
2019-11-05 11:08:36 +0900

    Initial commit

---
+++ Sources/SamplePackage/SamplePackage.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+public class Sample {
+
+    public init() {}
+
+    public var str: String = "お腹が減った！！！！"
+
+    public func getStr() -> String {
+        return self.str
+    }
+}
+
